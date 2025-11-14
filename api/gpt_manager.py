9c71025e3aac671e0420b8e2e600faf8b2942b65
Yeisson P <yeisson@shotflow.com>
2025-11-14 08:22:15 -0500

    exploration jupyer lab

--- api/gpt_manager.py
+++ api/gpt_manager.py
@@ -10,16 +10,9 @@ class GPTManager:
     def __init__(self, api_key=openai_key):
         self.client = OpenAI(api_key = api_key)
 
-    def prompt(self, input_text, params=None):
+    def prompt(self, input_text):
         response = self.client.responses.create(
             model = "gpt-5-nano",
             input = "this is a test, am I using my credits?",
         )
-        return response.json()
-
-    def post(self, endpoint, data=None):
-        url = f"{self.client}/{endpoint.lstrip('/')}"
-        headers = {"Authorization": f"Bearer {self.api_key}"} if self.api_key else {}
-        response = requests.post(url, headers=headers, json=data)
-        response.raise_for_status()
         return response.json()
